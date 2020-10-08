9c3f4e31981764e61cdbd5c420d86eab5f092eb0
HenryQuillin <henryquillin@gmail.com>
2020-10-08 11:53:56 -0500

    Added board

--- tick_tack_toe.py
+++ tick_tack_toe.py
@@ -9,23 +9,40 @@ Tick Tack Toe Board
 # Use .random to pick who goes first
 # Place first entry on the board
 # Gameplay
+    # insert an x or O into an array
+    # my_board_array[4] = O
 # Check for winner/tie condition
 # Win/lose message (with ASCII art)
 # Ask user to play again
 
 
 
-vertical = [' | | ']
-horizontal = ['- - -']
 
+check_for_x = ["X", "X", "X"]
+board = [
+    ["X", "X", " "],
+    ["X", "x", "X"],
+    [" ", " ", "0"]
+]
+print(f'''
+    0 1 2
+   _______
+0 | {board[0][0]}|{board[0][1]}|{board[0][2]} |
+  | -+-+- | 
+1 | {board[1][0]}|{board[1][1]}|{board[1][2]} |
+  | -+-+- |                                    
+2 | {board[2][0]}|{board[2][1]}|{board[2][2]} | 
+   -------
+''')
+def didiwin():
+    if board[0][0] == board [0][1] and board[0][0] == board[0][2]:
+        print(board[0][0], 'is the winner row')
 
-def board():
-    for i in range(3):
-        print(vertical[0])
-        if i < 2:
-            print(horizontal[0])
+didiwin()
 
-
+player1_col = input('Player 1 please enter you column ')
+player1_row = input('Player 1 please enter you row ')
+board[player1_col]
 
 print('Welcome to Tic-Tac- Toe')
 num_of_players = input('Choose number of players (0, 1 or 2): ') # Input for # of players
